ca2ad13864c090b0b3b72d3b59c2fd0efe73a60f
RyanErick <63329050+RyanErick@users.noreply.github.com>
2023-08-19 15:19:55 -0300

    Finalizado Led

---
+++ tcc/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    home: Tcc(),
+  ));
+}
+
+class Tcc extends StatelessWidget {
+
+  final ledstateController  = TextEditingController();
+  //final offController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text('Monitoramento de Energia'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/kanna.jpg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 35,
+              color: Colors.grey[350],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    DocumentReference docRef = FirebaseFirestore.instance.collection('led').doc('led');
+                    docRef.update({
+                      'ledstate' : ledstateController.text = 'on',
+                    }
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                    fixedSize: MaterialStateProperty.all(Size(100, 50)),
+                  ),
+                  child: const Text(
+                    'Ligar',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    DocumentReference docRef = FirebaseFirestore.instance.collection('led').doc('led');
+                    docRef.update({
+                      'ledstate' : ledstateController.text = 'off',
+                    }
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                    fixedSize: MaterialStateProperty.all(Size(100,50)),
+                  ),
+                  child: Text(
+                    'Desligar',
+                     style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
